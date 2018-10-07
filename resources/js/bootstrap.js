d535e1103bfbdea13a31b9aae93a29c4afc80b4e
Yin Kay Khine <42089861+YinKayKhine@users.noreply.github.com>
2018-10-07 17:39:24 +0630

    Add admin lte theme (#3)

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -12,7 +12,9 @@ try {
     window.$ = window.jQuery = require('jquery');
 
     require('bootstrap');
-} catch (e) {}
+} catch (e) {
+	require('admin-lte');
+}
 
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
---
+++ resources/views/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.admin_template')
+
+@section('content')
+    <div>
+        This is form
+    </div>
+
+@endsection
