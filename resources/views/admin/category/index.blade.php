9063ab9668131736611c8601f998150454437694
YinKayKhine <yinkaykhine91@gmail.com>
2018-10-22 21:50:58 +0630

    Adjust design and delete show page

--- resources/views/admin/category/index.blade.php
+++ resources/views/admin/category/index.blade.php
@@ -4,7 +4,7 @@
 
 <div class="row">
     <div class="col-md-offset-2 col-md-8">
-        <h3>Categories</h3>
+        <h3>List Category</h3>
         <a href="{{route('category.create')}}" class="btn btn-primary">Add New Category</a>
     </div>
     <div class="col-md-12">
@@ -12,8 +12,9 @@
         <table class="table table-stripped">
             <thead>
                 <tr>
-                    <th>Name</th>
-                    <th>Action</th>
+                    <th>Category Name</th>
+                    <th>Edit</th>
+                    <th>Delete</th>
                 </tr>
             </thead>
             <tbody>
@@ -21,7 +22,7 @@
                     <tr>
                         <td>{{ $category->category_name }}</td>
                         <td><a class="btn btn-primary" href="{{ route("category.edit", $category->id) }}">Edit</a></td>
-                        <td><a class="btn btn-primary" href="/category/delete/{{$category->id}}">Delete</a></td>
+                        <td><a class="btn btn-danger" href="/category/delete/{{$category->id}}">Delete</a></td>
                     </tr>
                 @endforeach
             </tbody>
